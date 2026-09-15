90fc02ac88d4751d0090e4200a61c63f8745c02e
Paulo Henrique <paulogas169@gmail.com>
2026-09-14 22:17:31 -0300

    Deletei a classe de teste
    Deletei a classe de teste do projeto principal

--- src/Teste.java
+++
@@ -1,7 +0,0 @@
-public class Teste {
-    public static void main (String[] args) {
-        System.out.println("Ol-a mundo!");
-        System.out.println("Ola!");
-        System.out.println("Marcelo");
-    }
-}
