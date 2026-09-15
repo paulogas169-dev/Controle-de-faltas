30c779cd63967f5a1655543c3908aaf0395b4fe9
Paulo Henrique <paulogas169@gmail.com>
2026-09-14 22:17:02 -0300

    Testando
    git funcionando com sucesso

--- src/Teste.java
+++ src/Teste.java
@@ -2,5 +2,6 @@ public class Teste {
     public static void main (String[] args) {
         System.out.println("Ol-a mundo!");
         System.out.println("Ola!");
+        System.out.println("Marcelo");
     }
 }
